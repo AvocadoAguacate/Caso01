837670a443a188e6dd988b6dff7476d45515cf40
Esteban <estebanguzra@gmail.com>
2023-02-15 09:17:21 -0600

    add tables, producers products storage inventary

--- caso01.sql
+++ caso01.sql
@@ -66,6 +66,62 @@ CREATE TABLE Units(
   unit_name NVARCHAR(255) NOT NULL
 );
 
+CREATE TABLE Persons(
+  id_person INT IDENTITY(1, 1) PRIMARY KEY,
+  person_name NVARCHAR(255) NOT NULL,
+  last_name NVARCHAR(255) NOT NULL,
+  email NVARCHAR(255) NOT NULL,
+  phone INT NOT NULL
+);
+
+CREATE TABLE Producers(
+  id_producer INT IDENTITY(1, 1) PRIMARY KEY,
+  producer_name NVARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE ProducerContacs(
+  id_producer_contact INT IDENTITY(1, 1) PRIMARY KEY,
+  id_producer INT FOREIGN KEY REFERENCES Producers(id_producer),
+  id_contact_type INT FOREIGN KEY REFERENCES ContactTypes(id_type),
+  contact NVARCHAR(255) NOT NULL,
+  contact_procedure NVARCHAR(255)
+);
+
+CREATE TABLE Products(
+  id_product INT IDENTITY(1, 1) PRIMARY KEY,
+  product_name NVARCHAR(255) NOT NULL,
+  id_unit INT FOREIGN KEY REFERENCES Units(id_unit),
+  min INT NOT NULL,
+  max INT NOT NULL
+);
+
+CREATE TABLE StorageSpaces(
+  id_storage_space INT IDENTITY(1, 1) PRIMARY KEY,
+  storage_name NVARCHAR(255) NOT NULL,
+  code NVARCHAR(255) NOT NULL,
+  storage_location GEOGRAPHY NOT NULL,
+  weight INT NOT NULL,
+  height INT NOT NULL,
+  deep INT NOT NULL,
+  id_storage_type INT FOREIGN KEY REFERENCES StorageTypes(id_Storage_type),
+  level_parent INT FOREIGN KEY REFERENCES StorageSpaces(id_storage_space),
+  level_number INT NOT NULL
+);
+
+CREATE TABLE InventaryLogs(
+  id_inventary_logs INT IDENTITY(1, 1) PRIMARY KEY,
+  id_product INT FOREIGN KEY REFERENCES Products(id_product),
+  post_time DATETIME DEFAULT GETDATE(),
+  quantity INT NOT NULL,
+  buy_price INT NOT NULL,
+  sell_price INT NOT NULL,
+  id_status INT FOREIGN KEY REFERENCES ProductStatus(id_status),
+  id_producer INT FOREIGN KEY REFERENCES Producers(id_producer),
+  id_storage_space INT FOREIGN KEY REFERENCES StorageSpaces(id_storage_space)
+);
+
+
+
 
 
 
